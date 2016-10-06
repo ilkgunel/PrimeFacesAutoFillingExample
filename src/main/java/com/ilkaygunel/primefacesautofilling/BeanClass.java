c4c30095d3b22e4514e2fe2b84eee6a7ae34ddab
İlkay Günel <ilkgunel93@gmail.com>
2016-10-06 19:15:03 +0300

    Updated

--- src/main/java/com/ilkaygunel/primefacesautofilling/BeanClass.java
+++ src/main/java/com/ilkaygunel/primefacesautofilling/BeanClass.java
@@ -2,30 +2,85 @@ package com.ilkaygunel.primefacesautofilling;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
 public class BeanClass {
+
     private List<String> cities;
-    private List<String> borough;
+    private List<String> boroughs;
+    private List<String> istanbulBorough;
+    private List<String> ankaraBorough;
+    private List<String> samsunBorough;
     
-    private BeanClass(){
+    private String selectedCity = "";
+    private String selectedBorough = "";
+
+    @PostConstruct
+    public void init() {
         cities = new ArrayList<>();
+        boroughs = new ArrayList<>();
+        istanbulBorough = new ArrayList<>();
+        ankaraBorough = new ArrayList<>();
+        samsunBorough = new ArrayList<>();
+        
         cities.add("istanbul");
         cities.add("ankara");
         cities.add("samsun");
-        borough = new ArrayList<>();
+
+        istanbulBorough.add("şişli");
+        istanbulBorough.add("eyüp");
+        istanbulBorough.add("beyoğlu");
+        
+        ankaraBorough.add("keçiören");
+        ankaraBorough.add("mamak");
+        ankaraBorough.add("sincan");
+        
+        samsunBorough.add("bafra");
+        samsunBorough.add("atakum");
+        samsunBorough.add("vezirköprü");
     }
 
-    public List<String> getBorough() {
-        return borough;
+    public List<String> getCities() {
+        return cities;
+    }
+    
+    public void fillBoroughList(){
+        if(selectedCity.equals("istanbul")){
+            boroughs = istanbulBorough;
+        }
+        else if(selectedCity.equals("ankara")){
+            boroughs = ankaraBorough;
+        }
+        else{
+            boroughs = samsunBorough;
+        }
     }
 
-    public void setBorough(List<String> borough) {
-        this.borough = borough;
+    public String getSelectedCity() {
+        return selectedCity;
     }
-    
-    
+
+    public void setSelectedCity(String selectedCity) {
+        this.selectedCity = selectedCity;
+    }
+
+    public List<String> getBoroughs() {
+        return boroughs;
+    }
+
+    public void setBoroughs(List<String> boroughs) {
+        this.boroughs = boroughs;
+    }
+
+    public String getSelectedBorough() {
+        return selectedBorough;
+    }
+
+    public void setSelectedBorough(String selectedBorough) {
+        this.selectedBorough = selectedBorough;
+    }
 }
